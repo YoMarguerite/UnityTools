977f08fdcde124f179355acc3eccadf1af9f6e7a
YoMarguerite <35686939+YoMarguerite@users.noreply.github.com>
2024-02-05 23:08:09 +0100

    Tab file

---
+++ Tab/Tab.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class Tab : MonoBehaviour, IPointerDownHandler
+    {
+        [SerializeField]
+        private TabManager tabManager;
+        private int indexManager;
+
+        [SerializeField]
+        private Image img;
+
+        [SerializeField]
+        private Sprite spriteSelect;
+
+        [SerializeField]
+        private Sprite spriteUnselect;
+
+        [SerializeField]
+        private GameObject screen;
+
+
+        public void Awake()
+        {
+            indexManager = tabManager.AddTab(this);
+        }
+
+        public void Select(bool select)
+        {
+            img.sprite = select ? spriteSelect : spriteUnselect;
+            screen.SetActive(select);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (screen != null && !screen.activeSelf)
+            {
+                tabManager.ChangeTab(this, indexManager);
+            }
+        }
+    }
+}
---
+++ Tab/TabManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class TabManager : MonoBehaviour
+    {
+        public List<Tab> tabs;
+        private int activeIndex;
+
+        private void Start()
+        {
+            CloseTabs();
+            if(tabs.Count > 0)
+            {
+                activeIndex = 0;
+                tabs[activeIndex].Select(true);
+            }
+        }
+
+        public int AddTab(Tab tab)
+        {
+            tabs.Add(tab);
+            return tabs.Count - 1;
+        }
+
+        public void ChangeTab(Tab tab, int index)
+        {
+            tabs[activeIndex].Select(false);
+            tab.Select(true);
+            activeIndex = index;
+        }
+
+        private void CloseTabs()
+        {
+            foreach (Tab tab in tabs)
+            {
+                tab.Select(false);
+            }
+        }
+    }
+}
